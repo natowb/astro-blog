cb8c4c5cabc70072b2e7a954cee955e26b0c32ac
Nato <me@natowb.dev>
2026-04-25 01:33:28 +1200

    add new pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
   site: "https://natowb.github.io",
-  base: "/my-repo",
+  integrations: [preact()],
 });
